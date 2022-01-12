f4dffef587e9b394a816805df57dffbc6e63a43c
Daro Macs <84887180+DaroMacs@users.noreply.github.com>
2022-01-11 19:23:59 -0800

    counter component

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const ItemCount = ({counter, setCounter, stock, setStock}) => {
+
+    const handleAdd = () => {
+        if(stock === 0 ){
+            alert('There is no more stock!')
+            return
+        }
+        setCounter( counter + 1)
+        setStock(stock - 1)
+        // console.log(counter, stock);
+    }
+    
+    const handleSubtract = () => {
+        if(counter === 1 ){
+            alert('There should be at least 1 item')
+            return
+        }
+        setCounter( counter - 1)
+        setStock(stock + 1)
+    }
+    console.log(counter, stock);
+
+    return (
+        <div className='row'>
+            <div className='col-2 d-flex align-items-center justify-content-center'>
+                <button 
+                type='button' 
+                className='btn btn-danger btn-sm'
+                onClick={() => handleSubtract()}
+                >
+                -1
+                </button>
+                <h2 className='mb-0 px-4 border'>{counter}</h2>
+                <button 
+                    type='button' 
+                    className='btn btn-warning btn-sm'
+                    onClick={() => handleAdd()}
+                >
+                +1
+                </button>
+            </div>
+            <div className="row">
+                <h4 className='pt-2'>{`Available Stock ${stock}`}</h4>
+            </div>
+        </div>
+    )
+}
+
+export default ItemCount
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ItemCount from './ItemCount'
+
 
 const ItemListContainer = () => {
+
+    const [counter, setCounter] = useState(0)
+    const [stock, setStock] = useState(5)
+
     return (
         <div className='row'>
             <div className="col-12 m-5">
-                <h3>Este es el componente ItemListContainer</h3> 
+                <ItemCount 
+                    counter = {counter}
+                    setCounter = {setCounter}
+                    stock = {stock}
+                    setStock = {setStock}
+                />
             </div>
         </div>
     )
